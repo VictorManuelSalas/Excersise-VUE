9aabe0c24ec0623098245c64a1791a00e947b1e6
VictorMNSalas <victorsalasnz@gmail.com>
2023-02-21 11:28:01 -0600

    send props from the component child to the father

--- vue3-bases/src/views/HomeView.vue
+++ vue3-bases/src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="home">
+    <EmitEvents :title="title_Emite" @onFire="eventEmmitedFromChildren"/>
+    <hr>
     <Suspense>
       <template #default>
-        <SuspenseFetch :title="title_Suspense" /> 
+        <SuspenseFetch :title="title_Suspense" />
       </template>
       <template #fallback>
         <img id="loader" src="../assets/loader.gif" alt="loader">
       </template>
     </Suspense>
     <hr>
-    <PeticionJson :title="title_Peticiones" /> 
+    <PeticionJson :title="title_Peticiones" />
     <hr>
     <ToDoList :title="title_ToDoList" />
     <hr>
@@ -34,6 +36,7 @@ import SimpleCounterCompositionAPI from '@/components/SimpleCounterCompositionAP
 import ToDoList from '@/components/ToDoList.vue';
 import PeticionJson from '@/components/PeticionJson.vue';
 import SuspenseFetch from '@/components/SuspenseFetch.vue';
+import EmitEvents from '@/components/EmitEvents.vue';
 
 export default {
   name: 'HomeView',
@@ -44,9 +47,13 @@ export default {
     SimpleCounterCompositionAPI,
     ToDoList,
     PeticionJson,
-    SuspenseFetch
+    SuspenseFetch,
+    EmitEvents
   },
   setup() {
+    const eventEmmitedFromChildren = (messange) =>{
+      console.log("Imprimir el mensaje del hijo desde el padre: ",messange);
+    }
     const title_refModel = 'Componente #1 - Referencias y V-Model';
     const title_incrementDecrement = 'Componente #2 - Incremento y Decremento Simple';
     const title_ReactCount = 'Componente #3 - ReactCount';
@@ -54,15 +61,18 @@ export default {
     const title_ToDoList = 'Componente #5 - To Do List Component';
     const title_Peticiones = 'Componente #6 - Peticiones Fetch Simples'
     const title_Suspense = 'Componente #7 - Peticiones Suspense Fetch'
+    const title_Emite = 'Componente #8 - Pasar Props del componente hijo al padre'
     return {
-      title_refModel,title_Peticiones,
-      title_incrementDecrement, title_ReactCount, title_SimpleCounterCompositionAPI,title_ToDoList,title_Suspense,
+      title_refModel, title_Peticiones, title_Emite,
+      title_incrementDecrement, title_ReactCount, 
+      title_SimpleCounterCompositionAPI, title_ToDoList, 
+      title_Suspense, eventEmmitedFromChildren,
     }
   }
 }
 </script>
 <style>
-#loader{
+#loader {
   width: 5%;
 }
 </style>
